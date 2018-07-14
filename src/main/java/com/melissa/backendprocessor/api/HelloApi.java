4debca5e93d0166e845f94579d1489d6098bf4d2
illi0715 <Ilia.Liubaikin@NetCracker.com>
2018-07-14 15:25:55 +0400

    add main class

--- src/main/java/com/melissa/backendprocessor/api/HelloApi.java
+++ src/main/java/com/melissa/backendprocessor/api/HelloApi.java
@@ -5,11 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Mill on 7/14/2018.
  */
-@Controller
+@RestController
 @RequestMapping
 public class HelloApi {
     @Autowired
